6e21a0124b4c47671679c9264302509ff8957db1
Michael <vmichaelkirkaldy@live.com>
2019-11-19 12:42:06 -0600

    Added to InterviewAlgos3.py

--- InterviewAlgos3.py
+++ InterviewAlgos3.py
@@ -1,6 +1,10 @@
+from random import shuffle
+
+
 #Fibnacci sequence
 #0,1,1,2,3,5,8,13,21,34,55,89,144
 #F(n) = F(n-1) + F(n-2)
+
 """
 def fibonacci(n):
 	#First number in fib sequence is 0
@@ -15,13 +19,15 @@ def fibonacci(n):
 
 for n in range (1, 101):
 	print(n, ":", fibonacci(n))
-
+"""
 #--------------------------------------------------------#
 
+"""
 FizzBuzz
 If a number is a multiple of 3 return fizz
 If it is a multiple of 5 return buzz
 If it is a multple of both return fizzbuzz
+"""
 
 
 def fizzbuzz():
@@ -34,14 +40,15 @@ def fizzbuzz():
 			print ("FizzBuzz")
 
 fizzbuzz()
-"""
+
 
 #-----------------------------------------------------------------------------#
+
 
 """
 Write a function that returns true if a string is a palindrone and false if it is not.
 A Palindrom is a word that is the same reverse as it is initially.
-
+"""
 
 def isPalindrone(str1):
 	rev_str = str1[::-1]
@@ -52,7 +59,7 @@ def isPalindrone(str1):
 
 print(isPalindrone("gag"))
 print(isPalindrone("Kamehameha"))
-"""
+
 
 #A function that returns multiple Palindomes.
 
@@ -68,6 +75,29 @@ def getPalindromes(input_str):
 
 sentence = "The concert starts at noon"
 getPalindromes(sentence)
+
+#----------------------------------------------------------------------#
+
+#randomize items in a list.
+
+def shuffleList(list1):
+	return shuffle(list1)
+
+
+x = ["matey", 1, 5, "Brink"]
+print(shuffleList(x))
+
+#-----------------------------------------------------------------------#
+
+#Multiply strings and return a string value.
+
+def multiplyStrings(str1, str2):
+	return str(int(str1) * int(str2))
+
+a = "56"
+b = "34"
+print(multiplyStrings(a,b))
+
 
 
 
